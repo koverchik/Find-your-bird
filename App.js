abcfb2df25c45a30b9532c1dd4b09b21038c43a3
koverchik <koverchik-work@yandex.ru>
2021-08-02 17:18:46 +0300

    add: ts packages  and  configuration file

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,7 +19,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+const Section = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -52,7 +45,7 @@ const Section = ({children, title}): Node => {
   );
 };
 
-const App: () => Node = () => {
+const App= () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
