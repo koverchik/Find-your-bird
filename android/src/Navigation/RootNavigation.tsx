c60002821df3aa7d65a3616a40682ec7bd3b71e7
koverchik <koverchik-work@yandex.ru>
2021-08-06 11:40:52 +0300

    add: SplashScreen

--- android/src/Navigation/RootNavigation.tsx
+++ android/src/Navigation/RootNavigation.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { SettingsNavigation } from './SettingsNavigation';
 import { HomeNavigation } from './HomeNavigation';
 import { TabNavigationScreen } from './types';
 import { TabNavigatorParamList } from './RootStackPrams';
+import SplashScreen from 'react-native-splash-screen';
 
 const Tab = createBottomTabNavigator<TabNavigatorParamList>();
-
 export const RootNavigation = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    });
+
     return (
         <NavigationContainer>
             <Tab.Navigator
