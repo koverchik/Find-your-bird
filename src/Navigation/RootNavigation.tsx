2d87bf0da29a3bb69e9e9e7baf41293c19d1b5c9
koverchik <koverchik-work@yandex.ru>
2021-08-10 10:45:31 +0300

    :add: icons on tab bar

--- src/Navigation/RootNavigation.tsx
+++ src/Navigation/RootNavigation.tsx
@@ -8,6 +8,9 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 import { useThemeAwareObject } from '../Theme/ThemeAwareObject.hook';
 import { createStyles } from './style';
 import { useTheme } from '../Theme/Theme.context';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faSlidersH } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 const Tab = createMaterialBottomTabNavigator<TabNavigatorParamList>();
 
@@ -22,6 +25,33 @@ export const RootNavigation = () => {
                 activeColor={theme.color.onPrimary}
                 inactiveColor={theme.color.primary}
                 barStyle={{ backgroundColor: Styles.container.backgroundColor }}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused }) => {
+                        if (route.name === TabNavigationScreen.HomeTab) {
+                            return (
+                                <FontAwesomeIcon
+                                    icon={faHome}
+                                    color={
+                                        focused == true
+                                            ? theme.color.onPrimary
+                                            : theme.color.primary
+                                    }
+                                />
+                            );
+                        } else {
+                            return (
+                                <FontAwesomeIcon
+                                    icon={faSlidersH}
+                                    color={
+                                        focused == true
+                                            ? theme.color.onPrimary
+                                            : theme.color.primary
+                                    }
+                                />
+                            );
+                        }
+                    }
+                })}
             >
                 <Tab.Screen
                     options={{
--- src/Theme/DefaultDark.theme.ts
+++ src/Theme/DefaultDark.theme.ts
@@ -9,10 +9,6 @@ const DEFAULT_DARK_COLOR_THEME: ColorTheme = {
     bright: '#602a3d'
 };
 
-const DEFAULT_DARK_SPACING_THEME: SpacingTheme = {
-    base: 8,
-    double: 16
-};
 export const DEFAULT_DARK_THEME_ID = 'default-dark';
 export const DEFAULT_DARK_THEME: Theme = {
     id: DEFAULT_DARK_THEME_ID,
