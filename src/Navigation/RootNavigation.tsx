2ca509ab22339edba097406f4ffd153a6e0192d1
koverchik <koverchik.o@gmail.com>
2021-08-19 17:48:51 +0300

    :add: flipper Hermes

--- src/Navigation/RootNavigation.tsx
+++ src/Navigation/RootNavigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { SettingsNavigation } from './SettingsNavigation';
 import { HomeNavigation } from './HomeNavigation';
 import { TabNavigationScreen } from './types';
@@ -10,16 +10,22 @@ import { createStyles } from './style';
 import { useTheme } from '../Theme/Theme.context';
 import { TabBarIcon } from './tabBarIcon';
 import { useTranslation } from 'react-i18next';
+import { useFlipper, useReduxDevToolsExtension } from '@react-navigation/devtools';
 
 const Tab = createMaterialBottomTabNavigator<TabNavigatorParamList>();
 
 export const RootNavigation = () => {
+  const navigationRef = useNavigationContainerRef();
+
+  useFlipper(navigationRef);
+  useReduxDevToolsExtension(navigationRef);
+
   const { theme } = useTheme();
   const Styles = useThemeAwareObject(createStyles);
   const { t } = useTranslation();
-  
+
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Tab.Navigator
         initialRouteName={TabNavigationScreen.HomeTab}
         activeColor={theme.color.onPrimary}
