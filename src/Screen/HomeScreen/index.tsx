74175176df5ac8d17a2087f9a06b0386279e6ed9
koverchik <koverchik.o@gmail.com>
2021-08-21 13:00:18 +0300

    add: pakage react-native-mapbox-gl/maps

--- src/Screen/HomeScreen/index.tsx
+++ src/Screen/HomeScreen/index.tsx
@@ -1,11 +1,16 @@
-import React, { FC, useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView, StyleSheet, TextInput } from 'react-native';
+import React, { FC, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { createStyles } from './style';
-import { HomeStackScreens, SettingsStackScreens } from '../../Navigation/types';
+import { HomeStackScreens } from '../../Navigation/types';
 import { useNavigation } from '@react-navigation/native';
 import { HomeScreenProps, StackNavigationPropNavigation } from './type';
 import { useThemeAwareObject } from '../../Theme/ThemeAwareObject.hook';
 import { useTranslation } from 'react-i18next';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+
+MapboxGL.setAccessToken(
+  'pk.eyJ1Ijoia292ZXJjaGlrIiwiYSI6ImNrc2tkdGxlaDFhcWgyb21idWhpbHF4a3EifQ.gTt3ny0ZCa1kWJtlYcE7mg',
+);
 
 export const HomeScreen: FC<HomeScreenProps> = () => {
   const navigation = useNavigation<StackNavigationPropNavigation>();
@@ -14,11 +19,31 @@ export const HomeScreen: FC<HomeScreenProps> = () => {
   const onPress = () => navigation.navigate(HomeStackScreens.Details, { userId: 12 });
   const { t } = useTranslation();
 
+  const [coordinates, setCoordinates] = useState([27.561831, 53.902284]);
+  const changeCoordigates = (e: GeoJSON.Feature<GeoJSON.Point>) =>
+    setCoordinates(e.geometry.coordinates);
   return (
     <View style={Styles.container}>
-      <TouchableOpacity style={Styles.button} onPress={onPress}>
+      <View style={Styles.containerMap}>
+        <MapboxGL.MapView
+          style={Styles.map}
+          localizeLabels={true}
+          logoEnabled={false}
+          onRegionDidChange={changeCoordigates}
+          onRegionIsChanging={changeCoordigates}
+          onRegionWillChange={changeCoordigates}
+          regionDidChangeDebounceTime={5000}
+          regionWillChangeDebounceTime={100}
+        >
+          <MapboxGL.PointAnnotation id="1" coordinate={coordinates} anchor={{ x: 0.5, y: 1 }}>
+            <View style={Styles.dot} />
+          </MapboxGL.PointAnnotation>
+          {/* <MapboxGL.ShapeSource id="symbolLocationSource" hitbox={{ width: 20, height: 20 }} /> */}
+        </MapboxGL.MapView>
+      </View>
+      {/* <TouchableOpacity style={Styles.button} onPress={onPress}>
         <Text style={Styles.text}>{t('components:buttonDitails')}</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 };
--- src/Screen/HomeScreen/style.ts
+++ src/Screen/HomeScreen/style.ts
@@ -1,10 +1,13 @@
-import { SafeAreaView, StyleSheet, ViewStyle } from 'react-native';
+import { StyleSheet, ViewStyle } from 'react-native';
 
 import { Theme } from '../../Theme/Theme.interface';
 
 type DetailsStyles = {
   container: ViewStyle;
   button: ViewStyle;
+  containerMap: ViewStyle;
+  map: ViewStyle;
+  dot: ViewStyle;
   text: Text;
 };
 
@@ -16,6 +19,19 @@ export const createStyles = (theme: Theme) => {
       backgroundColor: theme.color.primary,
       flexDirection: 'column',
       flex: 1,
+    },
+    containerMap: {
+      height: '100%',
+      width: '100%',
+      backgroundColor: theme.color.primary,
+    },
+    map: { flex: 1 },
+    dot: {
+      width: 25,
+      height: 25,
+      backgroundColor: theme.color.bright,
+      borderRadius: 25,
+      opacity: 0.6,
     },
     text: {
       color: theme.color.onPrimary,
