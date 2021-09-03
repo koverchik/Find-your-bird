961a3aaa638d015e6ba40d69e7e952eb43524763
koverchik <koverchik.o@gmail.com>
2021-09-03 16:10:37 +0300

    add: FlatList on page Airports

--- src/Screen/AirportsScreen/index.tsx
+++ src/Screen/AirportsScreen/index.tsx
@@ -1,5 +1,12 @@
 import React, { FC, useEffect, useState } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 import { createStyles } from './style';
 import { AirportsScreenProps } from './types';
 import { useThemeAwareObject } from '../../Theme/ThemeAwareObject.hook';
@@ -7,17 +14,55 @@ import { useTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from '../../Redux/hooks';
 import { airportsList } from '../../Redux/action/airports';
 import { getAirports } from '../../Redux/selectors';
+import { airportsListData } from './data';
 
 export const AirportsScreen: FC<AirportsScreenProps> = (props) => {
   const Styles = useThemeAwareObject(createStyles);
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(airportsList(props.route.params));
-  }, []);
-  const { airportsListData, pending } = useAppSelector(getAirports);
+  // useEffect(() => {
+  //   dispatch(airportsList(props.route.params));
+  // }, []);
+  const { pending } = useAppSelector(getAirports);
+
   console.log(airportsListData);
+
+  const renderItem = ({ item }) => (
+    <Item
+      title={item.name}
+      subtitle={item.municipalityName}
+      countryCode={item.countryCode}
+      location={item.location}
+      icao={item.icao}
+      iata={item.iata}
+    />
+  );
+
+  const Item = ({ title, subtitle, countryCode, location, icao, iata }) => (
+    <View style={Styles.wrapper}>
+      <View style={Styles.titleAirport}>
+        <Text style={Styles.textTitle}>{title}</Text>
+        <Text style={Styles.text}>{subtitle}</Text>
+        <Text style={Styles.text}>Country code: {countryCode}</Text>
+      </View>
+      <View style={Styles.wrapperCordAndCode}>
+        <View>
+          <Text style={Styles.text}>lat: {location.lat}</Text>
+          <Text style={Styles.text}>lon: {location.lon}</Text>
+        </View>
+        <View>
+          <Text style={Styles.text}>icao: {icao}</Text>
+          <Text style={Styles.text}>iata: {iata}</Text>
+        </View>
+      </View>
+      <View style={Styles.wrapperButton}>
+        <TouchableOpacity style={Styles.button}>
+          <Text style={Styles.text}>{t('components:textForDetailsScreen')}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 
   return pending ? (
     <ActivityIndicator
@@ -26,17 +71,12 @@ export const AirportsScreen: FC<AirportsScreenProps> = (props) => {
       style={Styles.activityIndicator}
     />
   ) : (
-    <View style={Styles.container}>
-      <Text> </Text>
-      {airportsListData.map((data, i) => {
-        return (
-          <View style={Styles.wrapper}>
-            <Text key={'airports' + i} style={Styles.item}>
-              {`${data.countryCode}  ${data.municipalityName}  ${data.iata} ${data.icao} ${data.shortName}`}
-            </Text>
-          </View>
-        );
-      })}
-    </View>
+    <SafeAreaView style={Styles.container}>
+      <FlatList
+        data={airportsListData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.icao}
+      />
+    </SafeAreaView>
   );
 };
--- src/Screen/AirportsScreen/style.ts
+++ src/Screen/AirportsScreen/style.ts
@@ -1,28 +1,63 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 import { Theme } from '../../Theme/Theme.interface';
 
 export const createStyles = (theme: Theme) => {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
-      backgroundColor: theme.color.primary,
-      flexDirection: 'column',
     },
     wrapper: {
-      height: 60,
-      width: '90%',
-      backgroundColor: theme.color.bright,
+      borderRadius: 10,
+      backgroundColor: theme.color.primary,
       margin: 10,
       padding: 10,
+      borderWidth: 2,
+      flex: 1,
+      justifyContent: 'space-between',
+    },
+    titleAirport: {
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    wrapperCordAndCode: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
     },
     item: {
       color: theme.color.primary,
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
     },
     activityIndicator: {
       alignItems: 'center',
       flex: 1,
       flexDirection: 'column',
+    },
+    title: {
+      fontSize: 32,
+    },
+    wrapperButton: {
+      alignItems: 'center',
+    },
+    button: {
+      margin: 5,
+      padding: 10,
+      backgroundColor: theme.color.bright,
+      width: 150,
+      borderRadius: 10,
+      borderBottomColor: theme.color.onPrimary,
+      borderWidth: 2,
+      alignItems: 'center',
+    },
+    textTitle: {
+      fontSize: 18,
+      color: theme.color.onPrimary,
+    },
+    text: {
+      color: theme.color.onPrimary,
     },
   });
   return styles;
