7fe38eb58c5f1ffae8df4c32a80888317f638905
koverchik <koverchik.o@gmail.com>
2021-09-17 08:16:16 +0300

    fix: auto start prettier after save

--- src/Screen/DetailsScreen/types.ts
+++ src/Screen/DetailsScreen/types.ts
@@ -2,8 +2,8 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
 import { RootStackParamList } from '@navigation/RootStackPrams';
 import { HomeStackScreens } from '@navigation/types';
-import {InitialDetailsAirportStateTypes} from "@redux/reducer/types";
-import {ListRenderItem} from "react-native";
+import { InitialDetailsAirportStateTypes } from '@redux/reducer/types';
+import { ListRenderItem } from 'react-native';
 
 export type DetailsScreenProps = {
   navigation: StackNavigationProp<RootStackParamList, HomeStackScreens.Details>;
@@ -11,9 +11,10 @@ export type DetailsScreenProps = {
   iata: string;
 };
 
-export  type DetailsScreenViewProps = {
-  airportData: InitialDetailsAirportStateTypes['airportData'],
-  linksAboutAirport:  () => ItemLinks[],
-  renderItem: ListRenderItem<ItemLinks>
-}
-export type ItemLinks = {link: string, name: string}
+export type DetailsScreenViewProps = {
+  airportData: InitialDetailsAirportStateTypes['airportData'];
+  linksAboutAirport: () => ItemLinks[];
+  renderItem: ListRenderItem<ItemLinks>;
+};
+
+export type ItemLinks = { link: string; name: string };
