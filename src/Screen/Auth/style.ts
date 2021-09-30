57f372cb6dc983af82912e20df614afc822ddd30
koverchik <koverchik.o@gmail.com>
2021-09-30 19:05:06 +0300

    fix: extra css

--- src/Screen/Auth/style.ts
+++ src/Screen/Auth/style.ts
@@ -3,11 +3,7 @@ import { Theme } from '@theme/Theme.interface';
 
 type DetailsStyles = {
   container: ViewStyle;
-  buttonDisable: ViewStyle;
-  buttonActive: ViewStyle;
   buttonGoogle: ViewStyle;
-  text: Text;
-  input: Text;
 };
 
 export const createStyles = (theme: Theme) => {
@@ -19,28 +15,10 @@ export const createStyles = (theme: Theme) => {
       flexDirection: 'column',
       flex: 1,
     },
-    text: {
-      color: theme.color.onPrimary,
-    },
     buttonGoogle: {
       width: 192,
       height: 48,
     },
-    buttonDisable: {
-      margin: 30,
-      padding: 15,
-      backgroundColor: theme.color.bright,
-      width: 150,
-      alignItems: 'center',
-    },
-    buttonActive: {
-      margin: 30,
-      padding: 15,
-      backgroundColor: theme.color.surface,
-      width: 150,
-      alignItems: 'center',
-    },
-    input: { height: 40, margin: 12, borderWidth: 1, padding: 10, width: 300 },
   });
   return styles;
 };
